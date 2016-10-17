80bb496e354d0debe4a17a35f818cff29947d1d0
Mikko Hilpinen <mikko_hilpinen@hotmail.com>
2016-10-17 15:19:51 +0300

    Added chapter parser

---
+++ TranslationEditor/USXChapterProcessor.swift
@@ -0,0 +1,67 @@
+//
+//  USXChapterProcessor.swift
+//  TranslationEditor
+//
+//  Created by Mikko Hilpinen on 17.10.2016.
+//  Copyright © 2016 Mikko Hilpinen. All rights reserved.
+//
+
+import Foundation
+
+class USXChapterProcessor: USXContentProcessor
+{
+	typealias Generated = Chapter
+	typealias Processed = Section
+	
+	
+	// ATTRIBUTES	---------
+	
+	private var index: Int
+	
+	
+	// INIT	-----------------
+	
+	init(index: Int)
+	{
+		self.index = index
+	}
+	
+	// Creates a new USX parser for chapter contents
+	// The parser should start after a chapter element
+	// the parser will stop at the next chapter element (or at next book / end of usx)
+	static func createChapterParser(caller: XMLParserDelegate, index: Int, targetPointer: UnsafeMutablePointer<[Chapter]>, using errorHandler: @escaping (USXParseError) -> ()) -> USXContentParser<Chapter, Section>
+	{
+		let parser = USXContentParser<Chapter, Section>(caller: caller, containingElement: .usx, lowestBreakMarker: .chapter, targetPointer: targetPointer, using: errorHandler)
+		parser.processor = AnyUSXContentProcessor(USXChapterProcessor(index: index))
+		
+		return parser
+	}
+	
+	
+	// USX PARSING	---------
+	
+	func getParser(_ caller: USXContentParser<Chapter, Section>, forElement elementName: String, attributes: [String : String], into targetPointer: UnsafeMutablePointer<[Section]>, using errorHandler: @escaping (USXParseError) -> ()) -> (XMLParserDelegate, Bool)?
+	{
+		// Delegates all para element parsing to section parsers
+		if elementName == USXContainerElement.para.rawValue
+		{
+			return (USXSectionProcessor.createSectionParser(caller: caller, targetPointer: targetPointer, using: errorHandler), true)
+		}
+		else
+		{
+			return nil
+		}
+	}
+	
+	func getCharacterParser(_ caller: USXContentParser<Chapter, Section>, into targetPointer: UnsafeMutablePointer<[Section]>, using errorHandler: (USXParseError) -> ()) -> XMLParserDelegate?
+	{
+		// Character parsing is only handled below para level
+		return nil
+	}
+	
+	func generate(from content: [Section], using errorHandler: (USXParseError) -> ()) -> Chapter?
+	{
+		// Wraps the collected sections into a character
+		return nil
+	}
+}
---
+++ TranslationEditor/USXSectionProcessor.swift
@@ -0,0 +1,83 @@
+//
+//  USXSectionParser.swift
+//  TranslationEditor
+//
+//  Created by Mikko Hilpinen on 10.10.2016.
+//  Copyright © 2016 Mikko Hilpinen. All rights reserved.
+//
+
+import Foundation
+
+// This USX Content processor is able to parse sections from USX data
+class USXSectionProcessor: USXContentProcessor
+{
+	typealias Generated = Section
+	typealias Processed = Paragraph
+	
+	
+	// ATTRIBUTES	---------
+	
+	private var contentParsed = false
+	
+	
+	// INIT	-----------------
+	
+	// Creates a new USX parser for section data
+	// The parser should start right after a chapter element or at the start of a section heading para element
+	// The parser will stop parsing at the start of the next chapter or section heading para element (or at the end of the usx element)
+	static func createSectionParser(caller: XMLParserDelegate, targetPointer: UnsafeMutablePointer<[Section]>, using errorHandler: @escaping (USXParseError) -> ()) -> USXContentParser<Section, Paragraph>
+	{
+		let parser = USXContentParser<Section, Paragraph>(caller: caller, containingElement: .usx, lowestBreakMarker: .chapter, targetPointer: targetPointer, using: errorHandler)
+		parser.processor = AnyUSXContentProcessor(USXSectionProcessor())
+		
+		return parser
+	}
+	
+	
+	// USX PARSING	---------
+	
+	func getParser(_ caller: USXContentParser<Section, Paragraph>, forElement elementName: String, attributes: [String : String], into targetPointer: UnsafeMutablePointer<[Paragraph]>, using errorHandler: @escaping (USXParseError) -> ()) -> (XMLParserDelegate, Bool)?
+	{
+		if elementName == USXContainerElement.para.rawValue
+		{
+			// Parses the para style in order to check when section headers are found
+			var sectionHeadingFound = false
+			if let styleAttribute = attributes["style"]
+			{
+				if ParaStyle.value(of: styleAttribute).isSectionHeadingStyle()
+				{
+					sectionHeadingFound = true
+				}
+			}
+			
+			// Section headers are used as markers between sections
+			if sectionHeadingFound && contentParsed
+			{
+				caller.stopsAfterCurrentParse = true
+				return nil
+			}
+			else
+			{
+				// Para content parsing is delegated to paragraph parser
+				contentParsed = true
+				return (USXParagraphProcessor.createParagraphParser(caller: caller, targetPointer: targetPointer, using: errorHandler), true)
+			}
+		}
+		else
+		{
+			return nil
+		}
+	}
+	
+	func getCharacterParser(_ caller: USXContentParser<Section, Paragraph>, into targetPointer: UnsafeMutablePointer<[Paragraph]>, using errorHandler: (USXParseError) -> ()) -> XMLParserDelegate?
+	{
+		// Character data is ignored. All character data should be inside para elements
+		return nil
+	}
+	
+	func generate(from content: [Paragraph], using errorHandler: (USXParseError) -> ()) -> Section?
+	{
+		// Wraps the paragraphs into a section
+		return Section(content: content)
+	}
+}
