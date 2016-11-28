4aff816af9a79d4f8c7aa40c80d04bf629441478
Mikko Hilpinen <mikko_hilpinen@hotmail.com>
2016-11-28 12:05:20 +0200

    Added JSON parsing to para, verse, chardata, etc. Created and ran a unit test for them as well

---
+++ TranslationEditor/JSONParseError.swift
@@ -0,0 +1,9 @@
+//
+//  JSONParseError.swift
+//  TranslationEditor
+//
+//  Created by Mikko Hilpinen on 28.11.2016.
+//  Copyright © 2016 Mikko Hilpinen. All rights reserved.
+//
+
+import Foundation
