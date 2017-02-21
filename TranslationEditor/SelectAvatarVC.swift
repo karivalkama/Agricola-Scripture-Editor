bf3337362e52c327c1ce0d4a5fd70cb070f97f30
Mikko Hilpinen <mikko_hilpinen@hotmail.com>
2017-02-21 15:56:01 +0200

    Added new VC. No implementation yet.

---
+++ TranslationEditor/SelectAvatarVC.swift
@@ -0,0 +1,53 @@
+//
+//  SelectAvatarVC.swift
+//  TranslationEditor
+//
+//  Created by Mikko Hilpinen on 21.2.2017.
+//  Copyright © 2017 Mikko Hilpinen. All rights reserved.
+//
+
+import UIKit
+
+// This view controller handles avatar selection and authorization
+class SelectAvatarVC: UIViewController
+{
+	// OUTLETS	-------------------
+	
+	@IBOutlet weak var avatarCollectionView: UICollectionView!
+	@IBOutlet weak var passwordField: UITextField!
+	@IBOutlet weak var loginButton: BasicButton!
+	@IBOutlet weak var passwordView: UIView!
+	
+	
+	// ATTRIBUTES	---------------
+	
+	
+	
+	// LOAD	-----------------------
+	
+    override func viewDidLoad()
+	{
+        super.viewDidLoad()
+    }
+	
+	
+	// ACTIONS	-------------------
+	
+	@IBAction func passwordFieldChanged(_ sender: Any)
+	{
+		// TODO: Enable or disable login button
+	}
+	
+	@IBAction func cancelPressed(_ sender: Any)
+	{
+		// TODO: Hide password field. Reset selection
+	}
+	
+	@IBAction func loginPressed(_ sender: Any)
+	{
+		// TODO: Check password, move to next view
+	}
+	
+	
+	// IMPLEMENTED METHODS	-------
+}
