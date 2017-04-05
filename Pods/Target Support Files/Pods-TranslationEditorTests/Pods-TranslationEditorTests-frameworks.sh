f1ddb45ef2041aea0a858fc2845dd80d080aa971
Mikko Hilpinen <mikko_hilpinen@hotmail.com>
2017-04-05 13:47:03 +0300

    Removed the xml parsing library since it wasn't used after all.

--- Pods/Target Support Files/Pods-TranslationEditorTests/Pods-TranslationEditorTests-frameworks.sh
+++ Pods/Target Support Files/Pods-TranslationEditorTests/Pods-TranslationEditorTests-frameworks.sh
@@ -84,14 +84,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/AEXML/AEXML.framework"
   install_framework "$BUILT_PRODUCTS_DIR/HTagView/HTagView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/QRCode/QRCode.framework"
   install_framework "$BUILT_PRODUCTS_DIR/QRCodeReader.swift/QRCodeReader.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SCrypto/SCrypto.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/AEXML/AEXML.framework"
   install_framework "$BUILT_PRODUCTS_DIR/HTagView/HTagView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/QRCode/QRCode.framework"
   install_framework "$BUILT_PRODUCTS_DIR/QRCodeReader.swift/QRCodeReader.framework"
