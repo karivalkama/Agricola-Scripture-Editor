d3b5ad6b154058af77332b4ba7023c80271f6bcd
Mikko Hilpinen <mikko_hilpinen@hotmail.com>
2017-05-18 13:16:02 +0300

    Fought with a million layout issues and unclear warnings

--- TranslationEditor/ConnectionUIView.swift
+++ TranslationEditor/ConnectionUIView.swift
@@ -7,8 +7,11 @@
 //
 
 import Foundation
+import AVFoundation
+import QRCodeReader
+import MessageUI
 
-class ConnectionUIView: UIView
+@IBDesignable class ConnectionUIView: CustomXibView, QRCodeReaderViewControllerDelegate, UITableViewDataSource, UITableViewDelegate, MFMailComposeViewControllerDelegate, ConnectionListener
 {
 	// OUTLETS	--------------
 	
@@ -24,6 +27,63 @@ class ConnectionUIView: UIView
 	@IBOutlet weak var sendEmailButton: BasicButton!
 	@IBOutlet weak var bookSelectionView: UIView!
 	@IBOutlet weak var bookSelectionTableView: UITableView!
+	
+	
+	// ATTRIBUTES	----------
+	
+	weak var viewController: UIViewController?
+	
+	private let canSendMail = MFMailComposeViewController.canSendMail()
+	private var targetTranslations = [Book]()
+	
+	// The reader used for capturing QR codes, initialized only when used
+	private lazy var readerVC: QRCodeReaderViewController =
+	{
+		let builder = QRCodeReaderViewControllerBuilder
+		{
+			$0.reader = QRCodeReader(metadataObjectTypes: [AVMetadataObjectTypeQRCode], captureDevicePosition: .back)
+		}
+		
+		return QRCodeReaderViewController(builder: builder)
+	}()
+	
+	
+	// INIT	--------------------
+	
+	override init(frame: CGRect)
+	{
+		super.init(frame: frame)
+		setupXib(nibName: "Connect")
+	}
+	
+	required init?(coder: NSCoder)
+	{
+		super.init(coder: coder)
+		setupXib(nibName: "Connect")
+	}
+	
+	override func awakeFromNib()
+	{
+		bookSelectionTableView.register(UINib(nibName: "LabelCell", bundle: nil), forCellReuseIdentifier: LabelCell.identifier)
+		bookSelectionTableView.dataSource = self
+		bookSelectionTableView.delegate = self
+		
+		do
+		{
+			if let projectId = Session.instance.projectId, let project = try Project.get(projectId)
+			{
+				targetTranslations = try project.targetTranslationQuery().resultObjects()
+			}
+		}
+		catch
+		{
+			print("ERROR: Failed to read target translation data. \(error)")
+		}
+		
+		updateStatus()
+		
+		sendEmailButton.isEnabled = canSendMail && !targetTranslations.isEmpty
+	}
 	
 	
 	// ACTIONS	--------------
@@ -49,33 +109,154 @@ class ConnectionUIView: UIView
 			P2PHostSession.stop()
 		}
 		
-		updateHostViewStatus()
-		updateJoinViewStatus()
-		updateHostInfoViewStatus()
+		updateStatus()
 	}
 	
 	@IBAction func joinSwitchChanged(_ sender: Any)
 	{
-		
+		if joinSwitch.isOn
+		{
+			guard let viewController = viewController else
+			{
+				print("ERROR: No view controller to host the QR scanner")
+				return
+			}
+			
+			// Presents a join P2P VC
+			readerVC.delegate = self
+			readerVC.modalPresentationStyle = .formSheet
+			viewController.present(readerVC, animated: true, completion: nil)
+		}
+		else
+		{
+			P2PClientSession.stop()
+			updateStatus()
+		}
 	}
 	
 	@IBAction func sendEmailButtonPressed(_ sender: Any)
 	{
-		
-	}
-	
-	@IBAction func closeButtonPressed(_ sender: Any)
-	{
-		
+		// When send email button is pressed, book selection is displayed
+		bookSelectionView.isHidden = false
+		sendEmailButton.isEnabled = false
 	}
 	
 	@IBAction func cancelSendButtonPressed(_ sender: Any)
 	{
+		// Hides the book selection
+		bookSelectionView.isHidden = true
+		sendEmailButton.isEnabled = true
+	}
+	
+	
+	// IMPLEMENTED METHODS	------
+	
+	func reader(_ reader: QRCodeReaderViewController, didScanResult result: QRCodeReaderResult)
+	{
+		reader.stopScanning()
+		print("STATUS: Scanned QR code result: \(result.value) (of type \(result.metadataType))")
+		viewController.or(reader).dismiss(animated: true, completion: nil)
 		
+		if let info = P2PConnectionInformation.parse(from: result.value)
+		{
+			print("STATUS: Starting P2P client session")
+			print("STATUS: \(info)")
+			
+			P2PClientSession.start(info)
+			updateStatus()
+		}
+		else
+		{
+			print("ERROR: Failed to parse connection information from \(result.value)")
+		}
+	}
+	
+	func reader(_ reader: QRCodeReaderViewController, didSwitchCamera newCaptureDevice: AVCaptureDeviceInput)
+	{
+		print("STATUS: Switched camera")
+	}
+	
+	func readerDidCancel(_ reader: QRCodeReaderViewController)
+	{
+		reader.stopScanning()
+		print("STATUS: QR Capture session cancelled")
+		viewController.or(reader).dismiss(animated: true, completion: nil)
+		
+		joinSwitch.isOn = false
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+	{
+		return 0
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+	{
+		return tableView.dequeueReusableCell(withIdentifier: LabelCell.identifier, for: indexPath)
+	}
+	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+	{
+		guard let viewController = viewController else
+		{
+			print("ERROR: No view controller to host the mail view.")
+			return
+		}
+		
+		do
+		{
+			let book = targetTranslations[indexPath.row]
+			let paragraphs = try ParagraphView.instance.latestParagraphQuery(bookId: book.idString).resultObjects()
+			
+			let usx = USXWriter().writeUSXDocument(book: book, paragraphs: paragraphs)
+			
+			guard let data = usx.data(using: .utf8) else
+			{
+				print("ERROR: Failed to generate USX data")
+				return
+			}
+			
+			let mailVC = MFMailComposeViewController()
+			mailVC.mailComposeDelegate = self
+			mailVC.addAttachmentData(data, mimeType: "application/xml", fileName: "\(book.code.code).usx")
+			mailVC.setSubject("\(book.code.name) - \(book.identifier) USX Export")
+			
+			sendEmailButton.isEnabled = true
+			bookSelectionView.isHidden = true
+			
+			viewController.present(mailVC, animated: true, completion: nil)
+		}
+		catch
+		{
+			print("ERROR: Failed to read translation data. \(error)")
+		}
+	}
+	
+	func onConnectionStatusChange(newStatus status: ConnectionStatus)
+	{
+		onlineStatusView.status = status
+		
+		// When in idle mode and client session is active, updates host info
+		if status == .done && P2PClientSession.isConnected
+		{
+			updateHostInfoViewStatus()
+		}
+	}
+	
+	func onConnectionProgressUpdate(transferred: Int, of total: Int, progress: Double)
+	{
+		onlineStatusView.updateProgress(completed: transferred, of: total, progress: progress)
 	}
 	
 	
 	// OTHER METHODS	-----
+	
+	private func updateStatus()
+	{
+		updateHostViewStatus()
+		updateJoinViewStatus()
+		updateHostInfoViewStatus()
+	}
 	
 	private func updateHostViewStatus()
 	{
@@ -127,19 +308,27 @@ class ConnectionUIView: UIView
 		
 		if P2PClientSession.isConnected, let clientSession = P2PClientSession.instance
 		{
-			do
+			// If correct info is already displayed, doesn't bother reading data again
+			if hostInfoView.isHidden
 			{
-				if let projectId = clientSession.projectId, let hostAvatarId = clientSession.hostAvatarId, let project = try Project.get(projectId), let hostAvatar = try Avatar.get(hostAvatarId), let hostInfo = try hostAvatar.info()
+				do
 				{
-					infoFound = true
-					hostImageView.image = hostInfo.image ?? #imageLiteral(resourceName: "userIcon")
-					hostNameLabel.text = "Joined \(hostAvatar.name)"
-					hostProjectLabel.text = "on project: \(project.name)"
+					if let projectId = clientSession.projectId, let hostAvatarId = clientSession.hostAvatarId, let project = try Project.get(projectId), let hostAvatar = try Avatar.get(hostAvatarId), let hostInfo = try hostAvatar.info()
+					{
+						infoFound = true
+						hostImageView.image = hostInfo.image ?? #imageLiteral(resourceName: "userIcon")
+						hostNameLabel.text = "Joined \(hostAvatar.name)"
+						hostProjectLabel.text = "on project: \(project.name)"
+					}
+				}
+				catch
+				{
+					print("ERROR: Failed to read host data from the database. \(error)")
 				}
 			}
-			catch
+			else
 			{
-				print("ERROR: Failed to read host data from the database. \(error)")
+				infoFound = true
 			}
 		}
 		
